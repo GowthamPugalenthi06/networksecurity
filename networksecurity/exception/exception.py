19cd8f83aafe05e2ce797223c159963607582688
NexgenAI024 <gowthamp1614@email.com>
2025-05-20 15:26:41 +0530

    Logger and exception handling completed

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,24 @@
+# src/exception.py
+import sys
+from networksecurity.logging import logger
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    return f"Error in file [{file_name}], line [{exc_tb.tb_lineno}]: {str(error)}"
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+        
+
+    def __str__(self):
+        return self.error_message
+
+if __name__ =="__main__":
+    try:
+        logger.logger.info("Enter the try block")
+        a = 10 / 0
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
