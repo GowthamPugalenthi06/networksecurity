a133162e309465407cce3385c0bb2df08cb44202
NexgenAI024 <gowthamp1614@email.com>
2025-05-20 22:18:57 +0530

    ETL pipeline is completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,24 +1,22 @@
-# src/exception.py
 import sys
 from networksecurity.logging import logger
 
-def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    return f"Error in file [{file_name}], line [{exc_tb.tb_lineno}]: {str(error)}"
-
 class NetworkSecurityException(Exception):
-    def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail)
+    def __init__(self,error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
         
-
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+    
     def __str__(self):
-        return self.error_message
-
-if __name__ =="__main__":
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        self.file_name, self.lineno, str(self.error_message))
+        
+if __name__=='__main__':
     try:
-        logger.logger.info("Enter the try block")
-        a = 10 / 0
+        logger.logging.info("Enter the try block")
+        a=1/0
+        print("This will not be printed",a)
     except Exception as e:
-        raise NetworkSecurityException(e, sys)
+           raise NetworkSecurityException(e,sys)
